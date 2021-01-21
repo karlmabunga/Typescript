84752825b09049430a87b8485538941c90f3fde3
Karl Mabunga <karlmabunga@yahoo.com>
2021-01-20 17:15:00 -0800

    Added interface to check for types

--- index.ts
+++ index.ts
@@ -2,4 +2,24 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((res) => console.log(res.data));
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((res) => {
+
+  // res.data has properties of:
+  // id
+  // title
+  // completed
+
+  const todo = res.data as Todo
+  const { id, title, completed } = todo;
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title with: ${title}
+  Is it finished? ${completed}
+  `);
+});
