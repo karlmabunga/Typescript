d5445d5421a7fce4847ea2aaf6e70a67b26d2c91
Karl Mabunga <karlmabunga@yahoo.com>
2021-01-24 23:56:53 -0800

    Added an arrays.ts to demonstrate TS with arrays

---
+++ annotations/arrays.ts
@@ -0,0 +1,14 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+
+// creating an inital array for strings but no data
+
+const strings: string[] = [];
+
+const dates: Date[] = [new Date(), new Date()];
+
+const carsByMake: string[][] = [
+  ['f150'],
+  ['corolla'],
+  ['camaro']
+];
+
