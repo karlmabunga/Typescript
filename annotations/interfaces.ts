7d874d85466d787369ba01d2f687df80a6256ab0
Karl Mabunga <karlmabunga@yahoo.com>
2021-01-25 23:40:21 -0800

    Added classes.ts and created a Vehicle class

--- annotations/interfaces.ts
+++ annotations/interfaces.ts
@@ -1,7 +1,4 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+interface Reportable {
   summary(): string;
 }
 
@@ -10,13 +7,22 @@ const oldCivic = {
   year: new Date(),
   broken: true,
   summary(): string {
-    return `Name is ${this.name}`;
+    return `The name is ${this.name}`;
   }
-
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.summary())
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`
+  }
+};
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary())
 }
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
+printSummary(drink);
