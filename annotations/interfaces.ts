392c5bb229ae816d781689ace6ea8d521c40ebb2
Karl Mabunga <karlmabunga@yahoo.com>
2021-01-25 21:58:49 -0800

    Added basic syntax for a function in an interface

--- annotations/interfaces.ts
+++ annotations/interfaces.ts
@@ -1,19 +1,22 @@
 interface Vehicle {
-  name: string,
-  year: number,
-  broken: boolean,
+  name: string;
+  year: Date;
+  broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name is ${this.name}`;
+  }
+
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`)
-  console.log(`Year: ${vehicle.year}`)
-  console.log(`Broken: ${vehicle.broken}`)
+  console.log(vehicle.summary())
 }
 
 printVehicle(oldCivic);
